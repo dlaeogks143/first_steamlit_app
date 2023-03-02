6a722823a07b5f10c8ee762078fa8374ca6eb0f2
dlaeogks143 <126813381+dlaeogks143@users.noreply.github.com>
2023-03-02 10:29:02 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title('My name is Daehan')
+streamlit.header('Breakfast Menu')
+streamlit.text('🍌 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥭 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞 Hard-Boiled Free-Range Egg')
+
