877c06a4f441db00033884fc6bfb0b9c8c07796d
dlaeogks143 <126813381+dlaeogks143@users.noreply.github.com>
2023-03-07 11:04:44 -0800

    Update feedback_monitor.py

--- feedback_monitor.py
+++ feedback_monitor.py
@@ -11,4 +11,12 @@ my_data_row = my_cur.fetchall()
 streamlit.text("HERE IS OUR COFFEE MENU")
 streamlit.dataframe(my_data_row)
 
+import pandas
 
+my_coffee_list = my_cur.execute("SELECT Item FROM COFFEE_MENU")
+my_coffee_list = my_coffee_list.set_index('Coffee')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+Item_selected = streamlit.selectbox("Pick your item:", list(my_coffee_list.index))
+
+Items_to_show = my_coffee_list.loc[fruits_selected]
