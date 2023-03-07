4ebd33d7a06bd5e844e6507b31fc12ae87dcc06a
dlaeogks143 <126813381+dlaeogks143@users.noreply.github.com>
2023-03-07 11:06:47 -0800

    Update feedback_monitor.py

--- feedback_monitor.py
+++ feedback_monitor.py
@@ -11,12 +11,16 @@ my_data_row = my_cur.fetchall()
 streamlit.text("HERE IS OUR COFFEE MENU")
 streamlit.dataframe(my_data_row)
 
-import pandas
+import pandas as pd
 
-my_coffee_list = my_cur.execute("SELECT Item FROM COFFEE_MENU")
-my_coffee_list = my_coffee_list.set_index('Coffee')
+# Use read_sql_query to execute the query and create a DataFrame
+my_coffee_list = pd.read_sql_query("SELECT Item FROM COFFEE_MENU", my_cur)
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-Item_selected = streamlit.selectbox("Pick your item:", list(my_coffee_list.index))
+# Set the index of the DataFrame to the "Item" column
+my_coffee_list = my_coffee_list.set_index('Item')
 
-Items_to_show = my_coffee_list.loc[fruits_selected]
+# Let's put a pick list here so they can pick the item they want to include 
+item_selected = streamlit.selectbox("Pick your item:", list(my_coffee_list.index))
+
+# Get the row of the selected item from the DataFrame
+item_to_show = my_coffee_list.loc[item_selected]
