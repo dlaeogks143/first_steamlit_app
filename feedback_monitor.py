8e441501b2db41bf0de7c3894b430c75dc37aaea
dlaeogks143 <126813381+dlaeogks143@users.noreply.github.com>
2023-03-07 10:52:58 -0800

    Update feedback_monitor.py

--- feedback_monitor.py
+++ feedback_monitor.py
@@ -3,12 +3,11 @@ import pandas
 
 streamlit.header('How was your drink? ☕')
 
-coffee_menu = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-coffee_menu = coffee_menu.set_index('Fruit')
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-coffee_selected = streamlit.selectbox("Pick some fruits:", list(coffee_menu.index))
-
-fruits_to_show = coffee_menu.loc[coffee_selected]
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * FROM COFFEE_MENU")
+my_data_row = my_cur.fetchone()
+streamlit.text("HERE IS OUR COFFEE MENU")
+streamlit.text(my_data_row)
 
 
